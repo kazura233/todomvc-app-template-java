59f74b8853f674c48d07d2ef979979732cfba0d7
kazura233 <kazira233@gmail.com>
2024-04-24 10:36:35 +0800

    init

---
+++ src/main/java/me/kazura233/todomvc/TodomvcApplication.java
@@ -0,0 +1,13 @@
+package me.kazura233.todomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodomvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/kazura233/todomvc/TodomvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kazura233.todomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodomvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
