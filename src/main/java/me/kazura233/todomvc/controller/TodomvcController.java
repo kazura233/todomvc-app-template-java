836dcc886475e6c1bb797aeb281ce0c7b7b140da
kazura233 <kazira233@gmail.com>
2024-04-24 14:06:33 +0800

    完善Controller

--- src/main/java/me/kazura233/todomvc/controller/TodomvcController.java
+++ src/main/java/me/kazura233/todomvc/controller/TodomvcController.java
@@ -7,13 +7,14 @@ import me.kazura233.todomvc.dto.TodomvcAddDTO;
 import me.kazura233.todomvc.dto.TodomvcDelDTO;
 import me.kazura233.todomvc.dto.TodomvcFindDTO;
 import me.kazura233.todomvc.dto.TodomvcModifyDTO;
+import me.kazura233.todomvc.entity.TodomvcEntity;
 import me.kazura233.todomvc.service.TodomvcService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -25,21 +26,38 @@ public class TodomvcController extends BaseController {
 
     @PostMapping("/find")
     public RestResult find(@RequestBody TodomvcFindDTO body) {
-        return success(new ArrayList<>());
+
+        List<TodomvcEntity> list = todomvcService.find(body.getKeyword(), body.getStatus());
+
+        return success(list);
     }
 
     @PostMapping("/add")
     public RestResult add(@RequestBody TodomvcAddDTO body) {
+        TodomvcEntity entity = new TodomvcEntity();
+        entity.setItem(body.getItem());
+
+        todomvcService.add(entity);
+
         return success();
     }
 
     @PostMapping("/modify")
     public RestResult modify(@RequestBody TodomvcModifyDTO body) {
+        TodomvcEntity entity = new TodomvcEntity();
+        entity.setId(body.getId());
+        entity.setItem(body.getItem());
+        entity.setStatus(body.getStatus());
+
+        todomvcService.modify(entity);
+
         return success();
     }
 
     @PostMapping("/del")
     public RestResult del(@RequestBody TodomvcDelDTO body) {
+        todomvcService.del(body.getId());
+
         return success();
     }
 }
--- src/main/java/me/kazura233/todomvc/service/TodomvcService.java
+++ src/main/java/me/kazura233/todomvc/service/TodomvcService.java
@@ -2,8 +2,13 @@ package me.kazura233.todomvc.service;
 
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import me.kazura233.todomvc.common.StatusEnum;
+import me.kazura233.todomvc.entity.TodomvcEntity;
 import me.kazura233.todomvc.repository.TodomvcRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -11,4 +16,21 @@ public class TodomvcService {
 
     @Resource
     private TodomvcRepository todomvcRepository;
+
+    public List<TodomvcEntity> find(String keyword, StatusEnum status) {
+
+        return new ArrayList<>();
+    }
+
+    public void add(TodomvcEntity entity) {
+
+    }
+
+    public void modify(TodomvcEntity entity) {
+
+    }
+
+    public void del(Long id) {
+
+    }
 }
